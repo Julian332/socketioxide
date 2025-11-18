3e885b11e1245eca54dfcccde5a14b1d55bd1779
DELL <jinguocao332@gmail.com>
2025-11-18 16:49:58 +0800

    debug

--- examples/axum-echo/axum_echo.rs
+++ examples/axum-echo/axum_echo.rs
@@ -4,7 +4,10 @@ use socketioxide::{
     extract::{AckSender, Data, SocketRef},
     SocketIo,
 };
-use tracing::info;
+use std::time::Duration;
+use tracing::{debug, info};
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::fmt::SubscriberBuilder;
 use tracing_subscriber::FmtSubscriber;
 
 async fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
@@ -27,7 +30,10 @@ async fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing::subscriber::set_global_default(FmtSubscriber::default())?;
+    tracing_subscriber::fmt::SubscriberBuilder::default()
+        .with_max_level(LevelFilter::DEBUG)
+        // .with_env_filter("trace")
+        .init();
 
     let (layer, io) = SocketIo::new_layer();
 
@@ -37,7 +43,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = axum::Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .layer(layer);
-
+    tokio::spawn(async move {
+        loop {
+            println!("{:?}", io.emit("heart_beat", "heart_beatasdada").await);
+            tokio::time::sleep(Duration::from_secs(5)).await;
+        }
+    });
     info!("Starting server");
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
